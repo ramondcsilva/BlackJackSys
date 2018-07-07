e350d397587cc6ca7c1c00f7a073770041bb27f5
ramon <ramon@LAPTOP-NT4RK8DA>
2018-07-07 09:25:29 -0300

    Commit controle de plágio.

--- src/blackjack/controller/BlackJackController.java
+++ src/blackjack/controller/BlackJackController.java
@@ -1,14 +1,3 @@
-/*  ******************************************************************************
- *  Autor: Sergio e Ramon
- *  Componente curricular: Módulo Integrador de Programação
- *  Concluido em: 
- *  Declaro que este código foi elaborado por mim de forma individual e não contém
- *  nenhum trecho de código de outro colega ou de outro autor, tais como provindos
- *  de livros e apostilas, e páginas ou documentos eletrônicos da Internet.
- *  Qualquer trecho de código de outra autoria que não a minha está destacado com
- *  uma citação para o autor e a fonte do código, e estou ciente que estes trechos
- *  não serão considerados para fins de avaliação.
- *  ******************************************************************************/
 package controller;
 
 import util.Iterator;
